b5aecbb6cda3f1067700ffc2db6e13737f2fcd21
chito <chito.pepsi@gmail.com>
2023-11-06 18:53:56 -0600

    se agrego el useFetch para llamar el api

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,36 @@
 import React, { useState } from "react";
-import icon from "../assets/icons/";
-export const Card = () => {
-  const [message, setMessage] = useState("");
-  const [messageId, setMessageId] = useState("");
-  return (
-    <>
-      <h4>ADVICE #{messageId}</h4>
-      <p>{message}</p>
-      <img src="" alt="" />
-    </>
-  );
+import { useFetch } from "../hooks/useFetch";
+import icon from "../assets/icons/icon-dice.svg";
+import divider from "../assets/icons/pattern-divider-desktop.svg";
+
+export const Card = ({ url }) => {
+  const { data, loading, error } = useFetch(url);
+
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (data) {
+    const parrafo = data.slip.advice;
+    const idParrafo = data.slip.id;
+
+    return (
+      <div className="centered-container">
+        <div>
+          <h4>ADVICE #{idParrafo}</h4>
+          <p>{parrafo}</p>
+          <img src={divider} alt="" />
+        </div>
+        <button className="btn" onClick={() => window.location.reload()}>
+          <img src={icon} alt="" />
+        </button>
+      </div>
+    );
+  }
+
+  return null;
 };
